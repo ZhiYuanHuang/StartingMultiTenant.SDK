83ce2b58476b05a7b331b26335838b61c038748a
zion <1459553674@qq.com>
2023-05-16 21:04:21 +0800

    updat

--- src/dotnet/StartingMultiTenantLib/StartingMultiTenantLib/StartingMutilTenantClient.cs
+++ src/dotnet/StartingMultiTenantLib/StartingMultiTenantLib/StartingMutilTenantClient.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace StartingMultiTenantLib
 {
     public class StartingMutilTenantClient
     {
         private readonly StartingMultiTenantClientOption _option;
+        private IRequestTenantExecutor _requestTenantExecutor = null;
+        private readonly object _lockObj = new object();
         public StartingMutilTenantClient(StartingMultiTenantClientOption clientOption) {
             _option = clientOption;
             
         }
 
+        public async Task<TenantDbConnsDto> GetTenantDbConns(string tenantDomain, string tenantIdentifier, string serviceIdentifier) {
+            var requestExecutor = getRequestTenantExecutor();
+            return await requestExecutor.GetTenantDbConns(tenantDomain,tenantIdentifier,serviceIdentifier);
+        }
 
+        private IRequestTenantExecutor getRequestTenantExecutor() {
+            if (_requestTenantExecutor == null) {
+                lock (_lockObj) {
+                    if (_requestTenantExecutor == null) {
+                        _requestTenantExecutor = _option.ResolveExecutor();
+                    }
+                }
+            }
+
+            return _requestTenantExecutor;
+        }
     }
 }
