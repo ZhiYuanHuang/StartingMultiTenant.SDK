102a33064a1095bc41792caa3c14306156f11d1f
huangzhiyuan <huangzhiyuan@seaeverit.com>
2023-05-22 14:06:51 +0800

    add WithStore

--- src/dotnet/StartingMultiTenantLib/StartingMultiTenantLib/ServiceMutiTenantStoreExtension.cs
+++ src/dotnet/StartingMultiTenantLib/StartingMultiTenantLib/ServiceMutiTenantStoreExtension.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Finbuckle.MultiTenant;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,14 +9,27 @@ namespace StartingMultiTenantLib
 {
     public static class ServiceMutiTenantStoreExtension
     {
-        public static IServiceCollection AddServiceMutiTenantStore(this IServiceCollection services, Action<ServiceMutiTenantStoreOption> optionAction) {
+        public static IServiceCollection AddServiceMutiTenantStore(this IServiceCollection services, Action<ServiceMutiTenantStoreOption> optionAction,ServiceLifetime serviceLifetime=ServiceLifetime.Scoped) {
             services.AddSingleton<ServiceMutiTenantStoreOption>((provider) => {
                 ServiceMutiTenantStoreOption option = new ServiceMutiTenantStoreOption();
                 optionAction(option);
                 return option;
             });
             services.AddScoped<ContextTenantDomain>();
-            return services.AddTransient<ServiceMutiTenantStore>();
+            services.Add(ServiceDescriptor.Describe(typeof(IMultiTenantStore<TenantDbConnsDto>),typeof(ServiceMutiTenantStore),serviceLifetime));
+            return services;
+        }
+
+        public static FinbuckleMultiTenantBuilder<TenantDbConnsDto> WithStore(this FinbuckleMultiTenantBuilder<TenantDbConnsDto> finbuckleMultiTenantBuilder, Action<ServiceMutiTenantStoreOption> optionAction, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        {
+            finbuckleMultiTenantBuilder.Services.AddSingleton<ServiceMutiTenantStoreOption>((provider) => {
+                ServiceMutiTenantStoreOption option = new ServiceMutiTenantStoreOption();
+                optionAction(option);
+                return option;
+            });
+            finbuckleMultiTenantBuilder.Services.AddScoped<ContextTenantDomain>();
+            finbuckleMultiTenantBuilder.WithStore<ServiceMutiTenantStore>(serviceLifetime);
+            return finbuckleMultiTenantBuilder;
         }
     }
 }
